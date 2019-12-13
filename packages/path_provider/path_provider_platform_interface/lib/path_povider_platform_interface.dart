5afbdd5dfc8d5ed24a39c871fe5dec2a8f241947
Francisco Magdaleno <franciscojma@google.com>
2019-12-13 10:37:34 -0800

    Experimenting

--- packages/path_provider/path_provider_platform_interface/lib/path_povider_platform_interface.dart
+++ packages/path_provider/path_provider_platform_interface/lib/path_povider_platform_interface.dart
@@ -119,56 +119,7 @@ abstract class PathProviderPlatform extends PlatformInterface {
     throw UnimplementedError('getExternalCacheDirectories() has not been implemented');
   }
 
-  /// Corresponds to constants defined in Androids `android.os.Environment` class.
-  ///
-  /// https://developer.android.com/reference/android/os/Environment.html#fields_1
-  enum StorageDirectory {
-    /// Contains audio files that should be treated as music.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_MUSIC.
-    music,
-
-    /// Contains audio files that should be treated as podcasts.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_PODCASTS.
-    podcasts,
-
-    /// Contains audio files that should be treated as ringtones.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_RINGTONES.
-    ringtones,
-
-    /// Contains audio files that should be treated as alarm sounds.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_ALARMS.
-    alarms,
-
-    /// Contains audio files that should be treated as notification sounds.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_NOTIFICATIONS.
-    notifications,
-
-    /// Contains images. See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_PICTURES.
-    pictures,
-
-    /// Contains movies. See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_MOVIES.
-    movies,
-
-    /// Contains files of any type that have been downloaded by the user.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_DOWNLOADS.
-    downloads,
-
-    /// Used to hold both pictures and videos when the device filesystem is
-    /// treated like a camera's.
-    ///
-    /// See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_DCIM.
-    dcim,
-
-    /// Holds user-created documents. See https://developer.android.com/reference/android/os/Environment.html#DIRECTORY_DOCUMENTS.
-    documents,
-  }
-
+  /// Corresponds to constants define
   /// Paths to directories where application specific data can be stored.
   /// These paths typically reside on external storage like separate partitions
   /// or SD cards. Phones may have multiple storage directories available.
